e8fc2c70a17ecc9afa7820b6660f4ebf7daea0ae
PT-70 <paulyjd@gmail.com>
2020-07-08 14:01:17 +0000

    added home app and template

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,5 +1,4 @@
 """boutique_ado URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
@@ -19,4 +18,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),
 ]
