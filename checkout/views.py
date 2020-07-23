b5633007f81ece9c9cf7f4c97f4fbcd32ab13f32
PT-70 <paulyjd@gmail.com>
2020-07-23 11:55:50 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-    }
+        'stripe_public_key': 'pk_test_51H81tkKhjWp5pbGsoahQ1NsBda4kFYckAGLSupKZUt4SR9vDlbRozE4gcoN485iK0RgQQiwTriAgJYiqU2wjO3z5005Mvo046i',	
+        'client_secret': 'test client secret',
+   }
 
     return render(request, template, context)
