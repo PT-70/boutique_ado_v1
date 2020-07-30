267253041b3eb061b8b452fac9ff4523db2cba82
PT-70 <paulyjd@gmail.com>
2020-07-30 16:43:20 +0000

    reversed

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -167,8 +167,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-#MEDIA_URL = '/media/'
-MEDIA_URL = "https://pt-70-boutique-ado" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
