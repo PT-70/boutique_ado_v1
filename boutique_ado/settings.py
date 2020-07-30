3bba9aeafe20f391392fb87969b2a05bdd4cd2c2
PT-70 <paulyjd@gmail.com>
2020-07-30 15:42:42 +0000

    fix error in settings.py allowwd hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'some value if your key is not in the 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ckz8780-boutique-ado.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pt-70-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
